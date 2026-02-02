22f310abdc353d2440d63604421d22e781f66c51
noa <noa.gomez.7e9@itb.cat>
2026-02-02 10:43:31 +0100

    1

--- src/Grup.kt
+++ src/Grup.kt
@@ -1,5 +1,12 @@
+
+
 class Grup {
     var nom : String
-    var llistaEstudiants : List<estudiant>
+    var llistaEstudiants : List<estudiant.estudiant>
     var tutor : Profesor
+
+    override fun toString(): String {
+        val contingutObjecte = ""
+        return contingutObjecte
+    }
 }
