3b154449ea1f2f1b9bc2415144111d617056aebc
noa.gomez.7e9 <noa.gomez.7e9@itb.cat>
2026-02-03 10:44:14 +0100

    ta47*

--- src/TA47/ObjecteGeometric.kt
+++ src/TA47/ObjecteGeometric.kt
@@ -5,5 +5,5 @@ open class ObjecteGeometric {
     private var y : Double = 0.0
     private var color : String = ""
 
-    constructor(x: Double, y: Double, color: String) {}
+    constructor(x: Double, y: Double, color: String)
 }
